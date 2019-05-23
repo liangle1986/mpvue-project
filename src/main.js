487c8681f38a86c7c95352b638a6ce8c8db95831
lianglele <liangle1986@126.com>
2019-05-23 21:07:46 +0800

    小程序程序

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+//全局处理重复页面跳转详情初始化,和详情跳详情返回的bug
+import MyPlugin from './minxins'
+Vue.use(MyPlugin)
+
+
+//引入store
+import store from './store/index'
+//把store挂载到全局
+Vue.prototype.$store = store;
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
---
+++ src/pages/index/index.vue
@@ -0,0 +1,177 @@
+<template>
+   <div class="index">
+    <div class="search">
+      <div @click="toMappage">{{cityName}}</div>
+      <div @click="toSearch">
+        <input type="text" placeholder="搜索小趣事">
+        <span class="icon"></span>
+      </div>
+    </div>
+    <div class="swiper">
+      <swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
+        <block v-for="(item, index) in banner " :key="index">
+          <swiper-item class="swiper-item">
+            <image :src="item.image_url" class="slide-image" />
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
+    <!--入区，区购，有趣，分享，最热-->
+    <div class="channel">
+      <div @click="categoryList(item.id)" v-for="(item, index) in channel" :key="index">
+        <img :src="item.icon_url" alt="">
+        <p>{{item.name}}</p>
+      </div>
+    </div>
+    <div class="brand">
+      <div @click="tobrandList" class="head">
+        小趣事
+      </div>
+      <div class="content">
+        <div @click="branddetail(item.id)" v-for="(item, index) in brandList" :key="index">
+          <div>
+            <p>{{item.name}}标题</p>
+          </div>
+          
+          <img :src="item.new_pic_url" alt="">
+         <span>{{item.name}}标题asdfasfasfasfn内容内容sadfasdfasdfsafasfdafasdfadfasdfasd</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+import amapFile from "../../utils/amap-wx";
+import { get } from '@/utils'
+import { mapState, mapMutations } from "vuex";
+export default {
+  onShow() {
+  },
+  computed: {
+    ...mapState(["cityName"])
+  },
+  mounted() {
+    this.getCityName();
+    this.getData();
+  },
+  data() {
+    return {
+      banner: [],
+      channel: [],
+      brandList: [],
+      newGoods: [],
+      hotGoods: [],
+      topicList: [],
+      newCategoryList: []
+    };
+  },
+  components: {},
+  methods: {
+    ...mapMutations(["update"]),
+    toMappage() {
+      var _this = this;
+      // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
+      wx.getSetting({
+        success(res) {
+          //如果没有同意授权,打开设置
+          if (!res.authSetting["scope.userLocation"]) {
+            wx.openSetting({
+              success: res => {
+                _this.getCityName();
+              }
+            });
+          } else {
+            wx.navigateTo({
+              url: "/pages/mappage/main"
+            });
+          }
+        }
+      });
+    },
+    getCityName() {
+      var _this = this;
+      var myAmapFun = new amapFile.AMapWX({
+        key: "e545e7f79a643f23aef187add14e4548"
+      });
+      myAmapFun.getRegeo({
+        success: function (data) {
+          //成功回调
+          console.log(data);
+          // data[0].regeocodeData.formatted_address
+          // _this.cityName = data[0].regeocodeData.formatted_address;
+          _this.update({ cityName: data[0].regeocodeData.formatted_address });
+        },
+        fail: function (info) {
+          //失败回调
+          console.log(info);
+          //如果用户拒绝授权
+          // 默认为北京
+          _this.cityName = "北京市";
+          _this.update({ cityName: "北京市" });
+        }
+      });
+    },
+    toSearch() {
+      wx.navigateTo({
+        url: "/pages/search/main"
+      });
+    },
+    async getData() {
+      const data = await get("/index/index");
+      this.banner = data.banner;
+      this.channel = data.channel;
+      this.brandList = data.brandList;
+      this.newGoods = data.newGoods;
+      this.hotGoods = data.hotGoods;
+      this.topicList = data.topicList;
+      this.newCategoryList = data.newCategoryList;
+    },
+    goodsDetail(id) {
+      wx.navigateTo({
+        url: "/pages/goods/main?id=" + id
+      });
+    },
+    categoryList(id) {
+      wx.navigateTo({
+        url: "/pages/categorylist/main?id=" + id
+      });
+    },
+    goodsList(info) {
+      if (info == "hot") {
+        wx.navigateTo({
+          url: "/pages/newgoods/main?isHot=" + 1
+        });
+      } else {
+        wx.navigateTo({
+          url: "/pages/newgoods/main?isNew=" + 1
+        });
+      }
+    },
+    topicdetail(id) {
+      wx.navigateTo({
+        url: "/pages/topicdetail/main?id=" + id
+      });
+    },
+    totopic() {
+      wx.navigateTo({
+        url: "/pages/topic/main"
+      });
+    },
+    tobrandList() {
+      wx.navigateTo({
+        url: "/pages/brandlist/main"
+      });
+    },
+    branddetail(id) {
+      wx.navigateTo({
+        url: "/pages/branddetail/main?id=" + id
+      });
+    }
+  },
+  created() { }
+};
+</script>
+<style lang='scss' scoped>
+@import "./style.scss";
+</style>
